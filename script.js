c2096cdb1f24811e90365947b938342b53e5efb9
pyaesonepsn <pyaesonepsn@gmail.com>
2022-03-14 01:00:56 +0630

    Added Section 2

--- script.js
+++ script.js
@@ -57,22 +57,39 @@ document.body.addEventListener('mouseleave', () => {
 })
 
 // Main Button
-const mainBtn = document.querySelector('.main-btn');
-let ripple;
+const mainBtns = document.querySelectorAll('.main-btn');
 
-mainBtn.addEventListener('mouseenter', e => {
-    const left = e.clientX - e.target.getBoundingClientRect().left;
-    const top = e.clientY - e.target.getBoundingClientRect().top;
+mainBtns.forEach((btn) => {
+    let ripple;
 
-    ripple = document.createElement('div')
-    ripple.classList.add("ripple");
-    ripple.style.left = `${left}px`;
-    ripple.style.top = `${top}px`;
-    mainBtn.prepend(ripple);
+    btn.addEventListener('mouseenter', e => {
+        const left = e.clientX - e.target.getBoundingClientRect().left;
+        const top = e.clientY - e.target.getBoundingClientRect().top;
+
+        ripple = document.createElement('div')
+        ripple.classList.add("ripple");
+        ripple.style.left = `${left}px`;
+        ripple.style.top = `${top}px`;
+        btn.prepend(ripple);
+    })
+
+    btn.addEventListener('mouseleave', () => {
+        btn.removeChild(ripple);
+    })
 })
 
-mainBtn.addEventListener('mouseleave', () => {
-    mainBtn.removeChild(ripple);
-})
 // End of Main Button
 
+// About Me Text
+const aboutMeText = document.querySelector(".about-me-text");
+const aboutMeTextContent = "I'm a web developer & I create websites with the best user experiences. Just contact me.";
+Array.from(aboutMeTextContent).forEach(char => {
+    const span = document.createElement('span');
+    span.textContent = char;
+    aboutMeText.appendChild(span);
+
+    span.addEventListener('mouseenter', (e) => {
+        e.target.style.animation = "aboutMeTextAnim 10s infinite"
+    })
+});
+// End of About Me Text
